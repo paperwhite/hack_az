663ae14e277a06f7754ba19a82175c37130648ab
Jeff Souza <jeffreymsouza@gmail.com>
2016-01-23 00:24:00 -0700

    basic google cloud sql skeleton

---
+++ app_mysql.py
@@ -0,0 +1,29 @@
+import MySQLdb
+import _mysql
+import os
+import webapp2
+
+class Page(webapp2.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/plain'
+
+    if (os.getenv('SERVER_SOFTWARE') and
+      os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')):
+      db = MySQLdb.connect(unix_socket='/cloudsql/your-project-id:your-instance-name', user='root')
+    else:
+      db = MySQLdb.connect(host='localhost', user='root')
+
+    db.query('SHOW VARIABLES')
+    result = db.store_result()
+    while True:
+      row = result.fetch_row()
+      if row:
+        self.response.write('%s\n' % str(row[0]))
+      else:
+        break
+
+    db.close()
+
+app = webapp2.WSGIApplication([
+    ('/mysql', Page),
+    ])
